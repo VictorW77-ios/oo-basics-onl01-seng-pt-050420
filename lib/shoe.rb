bcd0d236bbf3c7904f0d443290c82a431ddb2ad7
Victor Williams <victor.williams1030@gmail.com>
2020-05-19 14:36:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,4 +7,16 @@ class Shoe
   def brand 
     @brand
   end
+  
+  def color=(color)
+    @color = color
+  end
+  
+  def color 
+    @color
+  end
+  
+  def size=(size)
+    @size = size
+  end
 end
