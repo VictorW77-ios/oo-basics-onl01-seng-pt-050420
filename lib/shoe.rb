18b68fe1e981f0d969997e037757f80073ff37cb
Victor Williams <victor.williams1030@gmail.com>
2020-05-19 14:44:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,6 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   def initialize(brand)
     @brand = brand
@@ -6,38 +8,6 @@ class Shoe
   
   def brand 
     @brand
-  end
-  
-  def color=(color)
-    @color = color
-  end
-  
-  def color 
-    @color
-  end
-  
-  def size=(size)
-    @size = size
-  end
-  
-  def size 
-    @size
-  end
-  
-  def material=(material)
-    @material = material 
-  end
-  
-  def material 
-    @material
-  end
-  
-  def condition=(condition)
-    @condition = condition
-  end
-  
-  def condition
-    @condition
   end
   
   def cobble 
